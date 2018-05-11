7542eeb135086717608e8ceddacc8a20a8710f4f
othalla <othalla.lf@gmail.com>
2018-05-11 17:49:29 +0000

    [Refacto] move chrony in physical profile

---
+++ manifests/physical.pp
@@ -0,0 +1,12 @@
+class profiles::server {
+  class { '::chrony':
+    servers            => [
+      '0.fr.pool.ntp.org',
+      '1.fr.pool.ntp.org',
+      '2.fr.pool.ntp.org',
+      '3.fr.pool.ntp.org',
+    ],
+    chrony_password    => 'unset',
+    config_keys_manage => false,
+  }
+}
