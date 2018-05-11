1683afad76be994021f678b7836d713dc66c3de2
othalla <othalla.lf@gmail.com>
2018-05-11 12:48:24 +0000

    [Add] chrony default config

--- manifests/server.pp
+++ manifests/server.pp
@@ -1,4 +1,14 @@
 class profiles::server {
   include ::sudo
   include ::henrylf
+  class { '::chrony':
+    servers            => [
+      '0.fr.pool.ntp.org',
+      '1.fr.pool.ntp.org',
+      '2.fr.pool.ntp.org',
+      '3.fr.pool.ntp.org',
+    ],
+    chrony_password    => 'unset',
+    config_keys_manage => false,
+  }
 }
