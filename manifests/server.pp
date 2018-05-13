92d7e7d6fd1eec841d14f4c9ebb25b98174395df
othalla <othalla.lf@gmail.com>
2018-05-13 10:05:35 +0200

    [Add] ssh config & locales in server

--- manifests/server.pp
+++ manifests/server.pp
@@ -2,4 +2,22 @@ class profiles::server {
   include ::packages
   include ::sudo
   include ::henrylf
+  class { '::locales':
+    default_locale => 'en_US.UTF-8',
+    locales        => ['en_US.UTF-8 UTF-8'],
+  }
+  class { 'ssh::server':
+    storeconfigs_enabled => false,
+    options => {
+      'PasswordAuthentication' => 'no',
+      'PermitRootLogin'        => 'no',
+      'Port'                   => 22,
+      'AllowUsers'             => 'henrylf',
+      'PermitEmptyPasswords'   => 'no',
+      'ClientAliveInterval'    => 300,
+      'ClientAliveCountMax'    => 0,
+      'AuthenticationMethods'  => 'publickey',
+      'PubkeyAuthentication'   => 'yes',
+    },
+  }
 }
