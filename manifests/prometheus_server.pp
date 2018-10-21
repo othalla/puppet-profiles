a496a749cdecf3b493c8c546870e8140ec657963
othalla <othalla.lf@gmail.com>
2018-10-21 14:03:21 +0200

    Update prometheus server version to 2.4.3

--- manifests/prometheus_server.pp
+++ manifests/prometheus_server.pp
@@ -1,10 +1,11 @@
 class profiles::prometheus_server (
   $clients,
+  $version,
 ) {
 
   class { 'prometheus':
     manage_prometheus_server => true,
-    version => '2.0.0',
+    version => $version,
     alerts => { 'groups' => [{ 'name' => 'alert.rules', 'rules' => [{ 'alert' => 'InstanceDown', 'expr' => 'up == 0', 'for' => '5m', 'labels' => { 'severity' => 'page', }, 'annotations' => { 'summary' => 'Instance {{ $labels.instance }} down', 'description' => '{{ $labels.instance }} of job {{ $labels.job }} has been down for more than 5 minutes.' } }]}]},
     scrape_configs => [
       { 'job_name' => 'prometheus',
