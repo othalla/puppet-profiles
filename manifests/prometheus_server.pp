b8225cf9b005ffd20678884a900bd72464d7ba12
othalla <othalla.lf@gmail.com>
2019-05-09 00:08:29 +0200

    Add coredns to prometheus targets

--- manifests/prometheus_server.pp
+++ manifests/prometheus_server.pp
@@ -1,4 +1,5 @@
 class profiles::prometheus_server (
+  $dns_targets,
   $clients,
   $version,
 ) {
@@ -8,23 +9,44 @@ class profiles::prometheus_server (
     storage_retention => '1080h',
     alerts            => { 'groups' => [{ 'name' => 'alert.rules', 'rules' => [{ 'alert' => 'InstanceDown', 'expr' => 'up == 0', 'for' => '5m', 'labels' => { 'severity' => 'page', }, 'annotations' => { 'summary' => 'Instance {{ $labels.instance }} down', 'description' => '{{ $labels.instance }} of job {{ $labels.job }} has been down for more than 5 minutes.' } }]}]},
     scrape_configs    => [
-      { 'job_name' => 'prometheus',
-      'scrape_interval' => '15s',
-      'scrape_timeout'  => '10s',
-      'static_configs'  => [
-        { 'targets' => [ 'localhost:9090' ],
-        'labels'  => { 'alias'=> 'Prometheus'}
-        }
-      ]
+      {
+        'job_name' => 'prometheus',
+        'scrape_interval' => '15s',
+        'scrape_timeout'  => '10s',
+        'static_configs'  => [
+          {
+            'targets' => ['localhost:9090'],
+            'labels'  => {
+              'alias'=> 'Prometheus'
+            }
+          }
+        ]
       },
-      { 'job_name' => 'node',
-      'scrape_interval' => '15s',
-      'scrape_timeout'  => '10s',
-      'static_configs'  => [
-        { 'targets' => $clients,
-        'labels'  => { 'alias'=> 'Node'}
-        }
-      ]
+      {
+        'job_name' => 'node',
+        'scrape_interval' => '15s',
+        'scrape_timeout'  => '10s',
+        'static_configs'  => [
+          {
+            'targets' => $clients,
+            'labels'  => {
+              'alias'=> 'Node'
+            }
+          }
+        ]
+      },
+      {
+        'job_name' => 'coredns',
+        'scrape_interval' => '15s',
+        'scrape_timeout'  => '10s',
+        'static_configs'  => [
+          {
+            'targets' => $dns_targets,
+            'labels'  => {
+              'alias'=> 'CoreDNS'
+            }
+          }
+        ]
       }
     ],
   }
