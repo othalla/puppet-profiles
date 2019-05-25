f07bd15287c95d306b12385ba9e831a8151f4d43
othalla <othalla.lf@gmail.com>
2019-05-25 16:49:35 +0200

    Remove unused alertmanager config

--- manifests/prometheus_server.pp
+++ manifests/prometheus_server.pp
@@ -7,7 +7,6 @@ class profiles::prometheus_server (
   class { 'prometheus::server':
     version           => $version,
     storage_retention => '1080h',
-    alerts            => { 'groups' => [{ 'name' => 'alert.rules', 'rules' => [{ 'alert' => 'InstanceDown', 'expr' => 'up == 0', 'for' => '5m', 'labels' => { 'severity' => 'page', }, 'annotations' => { 'summary' => 'Instance {{ $labels.instance }} down', 'description' => '{{ $labels.instance }} of job {{ $labels.job }} has been down for more than 5 minutes.' } }]}]},
     scrape_configs    => [
       {
         'job_name' => 'prometheus',
